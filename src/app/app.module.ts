45fe2b0ba573f782cde6ebde933df15f62381657
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2020-10-16 00:01:40 +0200

    routes fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,8 +13,8 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { RouterModule, Routes } from "@angular/router";
 
 const appRoutes: Routes = [
-  { path: 'products', component: 'ProductListComponent' },
-  { path: 'products/:id', component: 'ProductPageComponent' },
+  { path: 'products', component: ProductListComponent },
+  { path: 'products/:id', component: ProductPageComponent },
   { path: '', redirectTo: 'products', pathMatch: 'full' }
 ]
 
